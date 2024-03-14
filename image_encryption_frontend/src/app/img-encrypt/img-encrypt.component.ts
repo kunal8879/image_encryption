e605a6aac2f36988229297c8c6b7968eb72b5837
Gaurang <gaurang.sant612@gmail.com>
2024-03-15 01:38:43 +0530

    Encrytion Completed

--- image_encryption_frontend/src/app/img-encrypt/img-encrypt.component.ts
+++ image_encryption_frontend/src/app/img-encrypt/img-encrypt.component.ts
@@ -23,21 +23,38 @@ export class ImgEncryptComponent {
       this.url = reader.result;
     }
 
-    document.getElementById('text')!.innerHTML = "<style display: none></style>";
-    // document.getElementById('browse-button')!.innerHTML = "<style display: none></style>";
-    document.getElementById('file')!.innerHTML = "<style display: none></style>";
+    const text = document.getElementById('text');
+    text!.style.display = "none";
+
+    const browseBtn = document.getElementById('browse-btn');
+    browseBtn!.style.display = "none";
+    
+    const file = document.getElementById('file');
+    file!.style.display = "none";
+
     const imgSize = document.getElementById('img-display');
     imgSize!.style.maxHeight = "200px";
     imgSize!.style.maxWidth = "400px";
 
-    // submit button code
-    // const submitBtn = document.getElementById('submit-btn');
-    // submitBtn!.style.display = "";
-
-
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn!.style.display = "block";
   }
 
-  
+  submitImg(){
+    const encKey = document.getElementById('enc-key');
+    encKey!.style.display = "block";
 
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn!.style.display = "none";
 
+    const fileUpload = document.getElementById('upload');
+    fileUpload!.style.display = "none";
+
+    const fileDownload = document.getElementById('download');
+    fileDownload!.style.display = "block";    
+  }
+
+  copyText(){
+    navigator.clipboard.writeText(this.enckey.toString());
+  }
 }
